3f65014c6bdaaa8c45ddc136b289e2f2a1584335
Felipe <felipper43@gmail.com>
2023-01-26 19:01:15 -0300

    Add: Cnpj (beta) e css

--- scripts/script.js
+++ scripts/script.js
@@ -2,10 +2,38 @@ let result = document.getElementById("resultado");
 let search = document.getElementById("searchData");
 let submit = document.getElementById("submit");
 
-submit.addEventListener("click", buscar);
-search.addEventListener("keypress", (key) => {
+let resultadoCnpj = document.getElementById("resultadoCnpj");
+let searchCnpj = document.getElementById("searchCnpj");
+let submitCnpj = document.getElementById("submitCnpj");
+
+async function buscarCnpj() {
+  let data = searchCnpj.value;
+  await fetch(`https://minhareceita.org/${data}`)
+    .then((res) => {
+      return res.json().then((res) => {
+        if (resultadoCnpj.childElementCount > 0) {
+          resultadoCnpj.removeChild(resultadoCnpj.firstChild);
+        }
+        let ul = document.createElement("ul");
+        resultadoCnpj.appendChild(ul);
+
+        Object.keys(res).forEach((el) => {
+          let li = document.createElement("li");
+          li.innerHTML = `${el.toUpperCase()}: ${res[el]}`;
+          ul.appendChild(li);
+        });
+      });
+    })
+
+    .catch((er) => {
+      alert(`Erro ${er}`);
+    });
+}
+
+submitCnpj.addEventListener("click", buscarCnpj);
+searchCnpj.addEventListener("keypress", (key) => {
   if (key.key == "Enter") {
-    buscar();
+    buscarCnpj();
   }
 });
 
@@ -32,3 +60,10 @@ async function buscar() {
       alert(`Erro ${er}`);
     });
 }
+
+submit.addEventListener("click", buscar);
+search.addEventListener("keypress", (key) => {
+  if (key.key == "Enter") {
+    buscar();
+  }
+});
